13b533d5668651bf27c2611e4f5864eb133b5f25
nvahelin@gmail.com <nvahelin@gmail.com>
2022-05-31 19:56:37 -0400

    2nd commit

--- src/github/gitAndGithub.java
+++ src/github/gitAndGithub.java
@@ -4,6 +4,8 @@ public class gitAndGithub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.print("today we learn github");
+		System.out.print("today is a very good day");
 
 	}
 
